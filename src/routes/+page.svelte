f40bc95b17b992ccb92ddaeddd1ea01450a0b210
PokeFred <Cedric.paelmke@gmx.de>
2024-03-03 21:13:04 +0100

    add ws

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
+<script lang="ts">
+    import { io } from "socket.io-client"
+
+    const socket = io()
+
+    socket.on("hello", (message) => {
+        console.log(message)
+    })
+</script>
+
 <div>Hello World</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
-import type { CommonServerOptions } from 'vite'
+import type { CommonServerOptions, ViteDevServer } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
+import { Server } from 'socket.io'
+import type { Socket } from 'socket.io'
 
 const serverConfig: CommonServerOptions = {
     host: '127.0.0.1',
@@ -11,5 +13,15 @@ const serverConfig: CommonServerOptions = {
 export default defineConfig({
     server: serverConfig,
     preview: serverConfig,
-    plugins: [sveltekit()],
+    plugins: [sveltekit(), {
+        name: "websocket-server",
+        configureServer(server: ViteDevServer): void {
+            if (!server.httpServer) return
+            const io: Server = new Server(server.httpServer)
+
+            io.on("connection", (socket: Socket) => {
+                socket.emit("hello", "world")
+            })
+        }
+    }],
 })
