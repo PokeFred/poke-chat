250e8f7c512d43a5d6501e44d232d9654a8d17cd
PokeFred <Cedric.paelmke@gmx.de>
2024-03-03 20:54:52 +0100

    add skeleton ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
     import '../app.css'
+
+    import { writable } from 'svelte/store'
+    import type { Writable } from 'svelte/store'
+    type Theme = 'skeleton'
+    const theme: Writable<Theme> = writable<Theme>('skeleton')
+    const darkmode: Writable<boolean> = writable<boolean>(true)
 </script>
 
-<slot />
+<div data-theme={$theme} class={$darkmode ? 'dark' : 'light'}>
+    <div class="bg-surface-50-900-token text-surface-800-100-token absolute left-0 top-0 h-screen w-screen overflow-hidden p-4">
+        <slot />
+    </div>
+</div>
